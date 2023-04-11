56de02d542ea6fb28d3bcc9caf0124487924c5a6
Ruslan P <111646522+nikopolzp@users.noreply.github.com>
2023-04-11 22:10:31 +0300

    add imports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+
 import { Profile } from 'components/profile/Profile';
+import user from './components/profile/user.json';
+
 import { Statistics } from 'components/statistics/Statistics';
+import data from './components/statistics/data.json';
+
 import { FriendList } from 'components/friendList/FriendList';
+import friends from 'components/friendList/friends.json';
+
 import { TransactionHistory } from 'components/transactionHistory/TransactionHistory';
+import transactions from './components/transactionHistory/transactions.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
